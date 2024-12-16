660ab8e7d02f8347191b0f30418296ed55ac5f07
atomic-zero <lkpanio25@gmail.com>
2024-12-16 08:15:34 +0800

    hahah

--- test.js
+++ test.js
@@ -2,10 +2,21 @@ const fs = require("fs");
 const login = require("./chatbox-fca-remake/package/index");
 
 // Simple bot. It will respond only if you say "test".
-login({appState: JSON.parse(fs.readFileSync('test.json', 'utf8'))}, (err, api) => {
+login({
+    appState: JSON.parse(fs.readFileSync('test.json', 'utf8'))}, (err, api) => {
     if (err) return console.error(err);
 
-    api.setOptions({ listenEvents: true});
+    api.setOptions({
+        forceLogin: true,
+        listenEvents: true,
+        logLevel: "silent",
+        updatePresence: true,
+        selfListen: false,
+        userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:118.0) Gecko/20100101 Firefox/118.0",
+        online: false,
+        autoMarkDelivery: false,
+        autoMarkRead: false
+    });
 
     const stopListening = api.listenMqtt((err, event) => {
         if (err) return console.error(err);
