119ce12f95cbd24de198ab57666050ff04430279
atomic-zero <lkpanio25@gmail.com>
2024-12-25 17:52:06 +0800

    hahah

--- script/0maintain.js
+++ script/0maintain.js
@@ -5,7 +5,7 @@ const configPath = path.join(__dirname, '../kokoro.json');
 module.exports["config"] = {
   name: "maintenance",
   version: "1.0.0",
-  role: 2,
+  role: 3,
   credits: "Markdevs69",
   info: "Turn on/off Maintenance mode.",
   type: "Admin",
@@ -13,10 +13,10 @@ module.exports["config"] = {
   cd: 5,
 };
 
-module.exports["run"] = async ({ api, event, args }) => {
+module.exports["run"] = async ({ api, event, args, font }) => {
     
   if (!fs.existsSync(configPath)) {
-    return api.sendMessage('Configuration file not found!', event.threadID, event.messageID);
+    return api.sendMessage(font.thin('Configuration file not found!'), event.threadID, event.messageID);
   }
 
   const config = JSON.parse(fs.readFileSync(configPath, 'utf-8'));
@@ -24,12 +24,12 @@ module.exports["run"] = async ({ api, event, args }) => {
   if (args[0] === 'on') {
     config.maintenance = { enabled: true };
     fs.writeFileSync(configPath, JSON.stringify(config, null, 2), 'utf-8');
-    api.sendMessage('Maintenance mode is now enabled.', event.threadID, event.messageID);
+    api.sendMessage(font.thin('Maintenance mode is now enabled.'), event.threadID, event.messageID);
   } else if (args[0] === 'off') {
     config.maintenance = { enabled: false };
     fs.writeFileSync(configPath, JSON.stringify(config, null, 2), 'utf-8');
-    api.sendMessage('Maintenance mode is now disabled.', event.threadID, event.messageID);
+    api.sendMessage(font.thin('Maintenance mode is now disabled.'), event.threadID, event.messageID);
   } else {
-    api.sendMessage('Incorrect syntax. Use: maintenance [on/off]', event.threadID, event.messageID);
+    api.sendMessage(font.thin('Incorrect syntax. Use: maintenance [on/off]'), event.threadID, event.messageID);
   }
 };
