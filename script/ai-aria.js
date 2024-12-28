4d8a39937ff0ed821a889e45c070dba5bc5e4bf9
atomic-zero <lkpanio25@gmail.com>
2024-12-29 07:26:41 +0800

    hahah

--- script/ai-aria.js
+++ script/ai-aria.js
@@ -63,7 +63,8 @@ async function queryOperaAPI(query, userId) {
       if (match) {
         const message = match[1]
           .replace(/\\n/g, '\n')
-          .replace(/\\ud83d\\ude0a|\\ud83d\\udc4b/g, '');
+          .replace(/\\ud83d\\ude0a|\\ud83d\\ude04|\\ud83d\\udc4b/g, '');
+          .replace(/\\u/g, ' ');
         result += message;
       }
     });
